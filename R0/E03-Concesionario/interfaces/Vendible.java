e95a3cf955ae73bc4f260c0b05718241e707067c
IsmaelNV <ismaelnavarrovalencia@gmail.com>
2025-10-14 20:07:07 +0200

    Reorganización: todos los ejercicios dentro de Pt2

--- R0/E03-Concesionario/interfaces/Vendible.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import model.Vehiculo;
-
-public interface Vendible {
-    boolean vender(Vehiculo vehiculo, String comprador);
-    double calcularPrecioFinal(Vehiculo vehiculo);
-    void mostrarDetallesVenta(Vehiculo vehiculo);
-}
